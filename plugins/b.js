4ae3a305b15834d89fa947117346e2383140b8c8
cocoa <cocoonut@qq.com>
2021-07-02 00:05:31 +0800

    first commit

---
+++ plugins/b.js
@@ -0,0 +1,34 @@
+class Toast {
+	constructor(opts) {
+		this.title = opts.title;
+		this.durations = opts.durations ? opts.durations : 3000;
+
+		this.el = document.createElement("div");
+		this.el.setAttribute("class", "a-toast");
+		this.el.innerHTML = this.title;
+		document.body.appendChild(this.el);
+
+		this.show();
+	}
+
+	show() {
+		this.el.classList.add("show");
+		setTimeout(() => {
+			this.el.classList.remove("show");
+			this.el.classList.add("hide");
+			this.el.addEventListener("animationend", () => {
+				this.el.remove();
+			});
+		}, this.durations);
+	}
+
+	// hide() {}
+
+	success() {}
+
+	error() {}
+
+	info() {}
+
+	warn() {}
+}

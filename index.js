96c3858b25fd452b06e04a3479799135f1aafe3c
coconut <cocoonut@qq.com>
2021-08-24 00:44:35 +0800

    -

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Toast from "@/library/Toast/index.js";
 import MessageBox from "@/library/MessageBox/index.js";
 // import ToolTip from "@/library/ToolTip/index.js";
 
-// window.Toast = Toast;
-// window.MessageBox = MessageBox;
+window.Toast = Toast;
+window.MessageBox = MessageBox;
 // window.ToolTip = ToolTip;
 export { Toast, MessageBox };
